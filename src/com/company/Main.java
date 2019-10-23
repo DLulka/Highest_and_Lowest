7b9bd775224df449892466fbc423e9ae4a6effb7
daniel.lulka <daniel.lulka@bbva.com>
2019-10-23 17:00:50 -0500

    First

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+    }
+
+    public static String highAndLow(String numbers) {
+        int i = 0, f = 1;
+        int max = 0, min = 0;
+        System.out.println(numbers.substring(1,2).equals(" "));
+        while (f < numbers.length()){
+        if(numbers.substring());
+        }
+        return "throw towel";
+    }
+
+}
